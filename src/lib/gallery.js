ab978b0a67dbe3ff17e7aeb7fa9825d12207b7ee
FO <jacob.s.easterling@gmail.com>
2026-04-30 16:02:55 -0400

    updated gallery page

--- src/lib/gallery.js
+++ src/lib/gallery.js
@@ -1,123 +1,72 @@
+import { allCards } from "@/data/cards";
+
 const CATEGORY_CONFIG = [
-  {
-    slug: "filter-feeders",
-    title: "Filter Feeders",
-    images: [
-      {
-        name: "Blue Whale",
-        src: "/images/cards/filter-feeders/blue-whale.png",
-      },
-      {
-        name: "Manta Ray",
-        src: "/images/cards/filter-feeders/manta-ray.png",
-      },
-    ],
-  },
-  {
-    slug: "apex",
-    title: "Apex",
-    images: [
-      {
-        name: "Bottlenose Dolphin",
-        src: "/images/cards/apex/bottlenose-dolpin.png",
-      },
-      {
-        name: "Bull Shark",
-        src: "/images/cards/apex/bull-shark.png",
-      },
-      {
-        name: "Great White",
-        src: "/images/cards/apex/great-white.png",
-      },
-      {
-        name: "Hammerhead",
-        src: "/images/cards/apex/hammerhead.png",
-      },
-      {
-        name: "Killer Whale",
-        src: "/images/cards/apex/killer-whale.png",
-      },
-      {
-        name: "Pilot Whale",
-        src: "/images/cards/apex/pilot-whale.png",
-      },
-      {
-        name: "Tiger Shark",
-        src: "/images/cards/apex/tiger-shark.png",
-      },
-    ],
-  },
-  {
-    slug: "predator",
-    title: "Predator",
-    images: [
-      {
-        name: "Moray Eel",
-        src: "/images/cards/predator/moray-eel.png",
-      },
-      {
-        name: "Spinner Dolphins",
-        src: "/images/cards/predator/spinner-dolpins.png",
-      },
-      {
-        name: "Thresher Shark",
-        src: "/images/cards/predator/thresher-shark.png",
-      },
-    ],
-  },
-  {
-    slug: "fish",
-    title: "Fish",
-    images: [
-      {
-        name: "Blue Tang",
-        src: "/images/cards/fish/blue-tang.png",
-      },
-      {
-        name: "Cleaner Wrasse",
-        src: "/images/cards/fish/cleaner-wrasse.png",
-      },
-      {
-        name: "French Angelfish",
-        src: "/images/cards/fish/french-angelfish.png",
-      },
-      {
-        name: "Picasso Triggerfish",
-        src: "/images/cards/fish/picasso-triggerfish.png",
-      },
-      {
-        name: "Spanish Hogfish",
-        src: "/images/cards/fish/spanish-hogfish.png",
-      },
-      {
-        name: "Spectacled Parrotfish",
-        src: "/images/cards/fish/spectacled-parrotfish.png",
-      },
-    ],
-  },
-  {
-    slug: "invertebrates",
-    title: "Invertebrates",
-    images: [
-      {
-        name: "Emerald Crab",
-        src: "/images/cards/invertebrates/emerald-crab.png",
-      },
-    ],
-  },
-  { slug: "coral", title: "Coral", images: [] },
-  {
-    slug: "support",
-    title: "Support",
-    images: [
-      {
-        name: "Coral Cement",
-        src: "/images/cards/support/coral-cement.png",
-      },
-    ],
-  },
+  { slug: "filter-feeders", title: "Filter Feeders", category: "filter-feeder" },
+  { slug: "apex", title: "Apex", category: "apex" },
+  { slug: "predator", title: "Predator", category: "predator" },
+  { slug: "fish", title: "Fish", category: "fish" },
+  { slug: "invertebrates", title: "Invertebrates", category: "invertebrate" },
+  { slug: "coral", title: "Coral", category: "coral" },
+  { slug: "structures", title: "Structures", category: "structure" },
+  { slug: "support", title: "Support", category: "support" },
 ];
 
+const AVAILABLE_IMAGE_PATHS = new Set([
+  "/images/cards/apex/bottlenose-dolpin.png",
+  "/images/cards/apex/bull-shark.png",
+  "/images/cards/apex/great-white.png",
+  "/images/cards/apex/hammerhead.png",
+  "/images/cards/apex/killer-whale.png",
+  "/images/cards/apex/pilot-whale.png",
+  "/images/cards/apex/tiger-shark.png",
+  "/images/cards/filter-feeders/blue-whale.png",
+  "/images/cards/filter-feeders/manta-ray.png",
+  "/images/cards/fish/blue-tang.png",
+  "/images/cards/fish/cleaner-wrasse.png",
+  "/images/cards/fish/french-angelfish.png",
+  "/images/cards/fish/picasso-triggerfish.png",
+  "/images/cards/fish/spanish-hogfish.png",
+  "/images/cards/fish/spectacled-parrotfish.png",
+  "/images/cards/invertebrates/emerald-crab.png",
+  "/images/cards/predator/moray-eel.png",
+  "/images/cards/predator/spinner-dolpins.png",
+  "/images/cards/predator/thresher-shark.png",
+  "/images/cards/support/coral-cement.png",
+]);
+
+const IMAGE_PATH_OVERRIDES = {
+  "bottlenose-dolphin": "/images/cards/apex/bottlenose-dolpin.png",
+  "killer-whales": "/images/cards/apex/killer-whale.png",
+  "green-moray-eel": "/images/cards/predator/moray-eel.png",
+  "spinner-dolphins": "/images/cards/predator/spinner-dolpins.png",
+};
+
+function galleryCard(card) {
+  const src = IMAGE_PATH_OVERRIDES[card.id] ?? card.image ?? null;
+  const hasPngImage = Boolean(
+    src?.toLowerCase().endsWith(".png") && AVAILABLE_IMAGE_PATHS.has(src)
+  );
+
+  return {
+    cardId: card.id,
+    name: card.name,
+    src,
+    hasImage: hasPngImage,
+    card,
+  };
+}
+
 export async function getGalleryData() {
-  return CATEGORY_CONFIG;
+  return CATEGORY_CONFIG.map((category) => ({
+    slug: category.slug,
+    title: category.title,
+    images: allCards
+      .filter(
+        (card) =>
+          card.category === category.category ||
+          card.kind === category.category
+      )
+      .sort((a, b) => a.sortOrder - b.sortOrder)
+      .map(galleryCard),
+  }));
 }
