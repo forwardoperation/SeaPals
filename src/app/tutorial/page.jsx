9f7acae4c923f3bd349a194793d1f7d379a61ec5
FO <jacob.s.easterling@gmail.com>
2026-06-27 13:03:35 -0400

    added basic tutorial

---
+++ src/app/tutorial/page.jsx
@@ -0,0 +1,11 @@
+import TutorialSimulator from "./TutorialSimulator";
+
+export const metadata = {
+  title: "Interactive Tutorial | SeaPals TCG",
+  description:
+    "Learn SeaPals through a guided 10 VP interactive tutorial game.",
+};
+
+export default function TutorialPage() {
+  return <TutorialSimulator />;
+}
